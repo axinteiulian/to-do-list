23ccb5f477514b8c87df300d799e6edf73eb392c
Iulian Axinte <axinte.iulian@gmail.com>
2019-11-06 21:10:22 +0300

    ce zici

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 	public static void main(String [] args){
 		System.out.println("Hello World!");
 		
-		System.out.println("Test Test Test");  
+		System.out.println("Test Test Test"); 
+		System.out.println("hai sa vedem daca functioneaza");
 	}
 }
