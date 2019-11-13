ef5b9c4ca7c55550239169c844c2c66b175def5f
Iulian Axinte <axinte.iulian@gmail.com>
2019-11-13 21:17:46 +0300

    studied classes and valuables

---
+++ racinggame/src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class Car {
+// instance variables
+    String name;
+    String color;
+    double mileage;
+    double maxSpeed;
+    double fuelLevel;
+    double travelDistance;
+    boolean running;
+    int doorCount;
+}
