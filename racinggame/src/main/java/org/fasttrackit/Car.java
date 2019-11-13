6ae83a9f842fe35f7253a58a34d53413ae8d40de
Iulian Axinte <axinte.iulian@gmail.com>
2019-11-13 21:30:16 +0300

    studied classes and valuables

--- racinggame/src/main/java/org/fasttrackit/Car.java
+++ racinggame/src/main/java/org/fasttrackit/Car.java
@@ -1,13 +1,7 @@
 package org.fasttrackit;
+// relatia de mostenire / inharitance or is a  relantionship
+public class Car extends {
 
-public class Car {
-// instance variables
-    String name;
-    String color;
-    double mileage;
-    double maxSpeed;
-    double fuelLevel;
-    double travelDistance;
-    boolean running;
+
     int doorCount;
 }
---
+++ racinggame/src/main/java/org/fasttrackit/vehicle.java
@@ -0,0 +1,13 @@
+package org.fasttrackit;
+
+public class vehicle {
+
+    // instance variables
+    String name;
+    String color;
+    double mileage;
+    double maxSpeed;
+    double fuelLevel;
+    double travelDistance;
+    boolean running;
+}
