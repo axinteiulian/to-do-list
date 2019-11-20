a1bd8a9e73a56b5b774b7c82c48a7f3ce6096e4f
Iulian Axinte <axinte.iulian@gmail.com>
2019-11-20 20:26:47 +0300

    studied methods

--- racinggame/src/main/java/org/fasttrackit/Vehicle.java
+++ racinggame/src/main/java/org/fasttrackit/Vehicle.java
@@ -14,4 +14,25 @@ public class Vehicle {
     double travelDistance;
     boolean running;
 
+    public double accelerate(double speed, double durationInHours) {
+
+        // partea de sus se numeste semnatura metodei
+        System.out.println(name + " is accelerating with" + speed + durationInHours + "h");
+
+        double distance = speed * durationInHours;
+        travelDistance = travelDistance + distance;
+        System.out.println("Total travel distance "+ travelDistance);
+
+        //same result as the statement about
+//        travelDistance += distance;
+
+        double usedfuel = distance * travelDistance / 100;
+        System.out.println("usedfuel :" + usedfuel);
+
+        fuelLevel -= usedfuel;
+        System.out.println("Remaing fuel level : "+ fuelLevel);
+        return distance;
+
+    }
+
     }
